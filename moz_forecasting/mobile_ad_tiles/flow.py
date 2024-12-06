aae9bd1fdbbf7855db696ce6e7bbd9ecaf24f710
Jared Snyder <jsnyder@mozilla.com>
2024-12-05 20:02:08 -0600

    stupid null check mistake

--- moz_forecasting/mobile_ad_tiles/flow.py
+++ moz_forecasting/mobile_ad_tiles/flow.py
@@ -85,7 +85,7 @@ class MobileAdTilesForecastFlow(FlowSpec):
         self.excluded_advertisers = self.config_data["excluded_advertisers"]
 
         logging.info(f"Forecast month input as: {self.set_forecast_month}")
-        if self.set_forecast_month or self.set_forecast_month == "":
+        if not self.set_forecast_month or self.set_forecast_month == "":
             self.first_day_of_current_month = datetime.today().replace(day=1)
         else:
             self.first_day_of_current_month = datetime.strptime(
